ee9572f362c3527d3708d3c55099c1fef03389a8
Sanketh B K <sanketh.bk@razorpay.com>
2024-11-04 01:34:06 +0530

    migration to minimal mistakes failed attempt

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "My Resources for Learning Operating Systems and Computer Architecture",
+        "excerpt":"I have previously studied operating systems as a part of my university curriculum, i’ve realized it’s time to revisit the fundamentals and go beyond. I have decided to start with some popular books. I will add more resources as i go along. Text Books Operating Systems Three Easy Pieces Linux...","categories": ["operating-systems","learning-resources"],
+        "tags": [],
+        "url": "/blog/operating-systems/learning-resources/my-study-resources",
+        "teaser": null
+      },{
+        "title": "Components of CPU",
+        "excerpt":"Before learning Assembly, I think it would be useful to learn a bit about different components of CPU in general. If we think of CPU as a black box its main function is to fetch instructions from RAM which are in the form of machine code and execute them. Components...","categories": ["cpu"],
+        "tags": [],
+        "url": "/blog/cpu/components-of-cpu",
+        "teaser": null
+      },{
+        "title": "Key Differences between 32-bit and 64-bit CPU architectures",
+        "excerpt":"The terms 32 bit and 64 bit specifically relate to the size of the data and address registers within the CPU, which determines the maximum amount of memory that can be directly accessed and the range of values that can be processed. Registers and Data Width: Since all calculations take...","categories": ["cpu"],
+        "tags": [],
+        "url": "/blog/cpu/processor-bit-size",
+        "teaser": null
+      },{
+        "title": "Measuring CPU Performance",
+        "excerpt":"CPU Manufacturers publish several metrics related to CPU like clock speed, number of cores, cache sizes, ISA, performance per Watt, number of transistors and more. Measuring CPU performance is complex, and it cannot be summarized by a single metric. In this post, I’ll explore each of these metrics and discuss...","categories": ["cpu"],
+        "tags": [],
+        "url": "/blog/cpu/measuring-cpu-performance",
+        "teaser": null
+      },{
+        "title": "The Fetch Decode Execute Cycle",
+        "excerpt":"The Fetch-decode-execute cycle or instruction cycle is how CPU executes programs. During this cycle, the CPU retrieves an instruction from memory (fetch), interprets what action is required (decode), and then carries out the necessary operations to complete the instruction (execute). This cycle is crucial for the CPU to perform any...","categories": ["cpu"],
+        "tags": [],
+        "url": "/blog/cpu/the-fetch-decode-execute-cycle",
+        "teaser": null
+      },{
+        "title": "Overview of MIPS Assembly",
+        "excerpt":"MIPS (Microprocessor without Interlocked Pipeline Stages) assembly is one of the RISC ISA’s. It was developed in the early 1980s at Stanford University by Professor John L. Hennessy. MIPS is widely used in academic research and industry, particularly in computer architecture courses due to its straightforward design and in various...","categories": ["cpu","assembly"],
+        "tags": [],
+        "url": "/blog/cpu/assembly/overview-of-mips-assembly",
+        "teaser": null
+      },{
+        "title": "Representation of Negative Numbers in Hardware",
+        "excerpt":"Representing negative numbers in binary poses unique challenges due to the inherent nature of binary systems. Unlike decimal systems, which can easily use a minus sign to indicate negative values, binary systems must encode this information within a fixed number of bits. This requirement leads to various methods of representation,...","categories": ["cpu"],
+        "tags": [],
+        "url": "/blog/cpu/representation-of-negative-numbers",
+        "teaser": null
+      }]
